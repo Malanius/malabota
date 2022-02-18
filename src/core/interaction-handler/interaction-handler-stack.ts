6a68fe6db753443f352057b281afcb2d84297d73
Malanius Privierre <malaniusprivierre@gmail.com>
2022-02-18 16:20:16 +0100

    ✨ (core): Create stack for interaction handler

---
+++ src/core/interaction-handler/interaction-handler-stack.ts
@@ -0,0 +1,95 @@
+import * as cdk from 'aws-cdk-lib';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as pylambda from '@aws-cdk/aws-lambda-python-alpha';
+import { Construct } from 'constructs';
+
+import { AppInfo } from '../../common/app-info';
+import { Models } from './api/models';
+
+export interface InteractionHandlerStackProps extends cdk.StackProps, AppInfo {}
+
+export class InteractionHandlerStack extends cdk.Stack {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: InteractionHandlerStackProps
+  ) {
+    super(scope, id, props);
+
+    const { appName, appEnv, module } = props;
+
+    const interactionLambda = new pylambda.PythonFunction(
+      this,
+      'InteractionLambda',
+      {
+        functionName: `${appName}-${appEnv}-${module}-interaction-handler`,
+        entry: 'lamdbas/core/interaction-handler/',
+        runtime: lambda.Runtime.PYTHON_3_9,
+        layers: [
+          lambda.LayerVersion.fromLayerVersionArn(
+            this,
+            'PowertoolsLayer',
+            `arn:aws:lambda:${this.region}:017000801446:layer:AWSLambdaPowertoolsPython:11`
+          ),
+        ],
+      }
+    );
+
+    // API
+    const api = new apigateway.RestApi(this, 'apigateway', {
+      restApiName: `${appName}-${appEnv}`,
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowHeaders: apigateway.Cors.DEFAULT_HEADERS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+      },
+      deployOptions: {
+        stageName: appEnv,
+        loggingLevel: apigateway.MethodLoggingLevel.INFO,
+        tracingEnabled: true,
+      },
+    });
+
+    // Request templates
+    const requestTemplates = {
+      'application/json': '{ "statusCode": "200" }',
+    };
+
+    const requestValidator = api.addRequestValidator('RequestValidator', {
+      validateRequestBody: true,
+      validateRequestParameters: false,
+    });
+
+    const models = new Models(this, 'Models', { api });
+
+    // /events
+    const eventsResource = api.root.addResource('events');
+
+    const postEventIntegration = new apigateway.LambdaIntegration(
+      interactionLambda,
+      {
+        requestTemplates,
+      }
+    );
+
+    // POST /events
+    eventsResource.addMethod('POST', postEventIntegration, {
+      operationName: 'Send event',
+      requestValidator,
+      requestModels: {
+        'application/json': models.interactionRequest,
+      },
+      methodResponses: [
+        {
+          statusCode: '200',
+          responseModels: { 'application/json': models.interactionResponse },
+        },
+        {
+          statusCode: '401',
+          responseModels: { 'application/json': apigateway.Model.ERROR_MODEL},
+        },
+      ],
+    });
+  }
+}
